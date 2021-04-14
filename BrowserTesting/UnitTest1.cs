0498db24a2408dae2cbaa0b2f0d10140ce559c1a
Radar-zxc <57041243+Radar-zxc@users.noreply.github.com>
2021-04-14 13:36:26 +0300

    Добавьте файлы проекта.

---
+++ BrowserTesting/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+namespace BrowserTesting
+{
+    public class SearchInGoogle
+    {
+        private IWebDriver driver;
+        private readonly By Input_Search = By.XPath("//input[@class='gLFyf gsfi']");
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.ru/");
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var enter = driver.FindElement(Input_Search);
+            enter.Click();
+            enter.SendKeys("ABOBA");
+            enter.SendKeys(Keys.Enter);
+            Thread.Sleep(2000);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
