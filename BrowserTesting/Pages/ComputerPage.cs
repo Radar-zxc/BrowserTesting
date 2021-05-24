d894f308437c3921abe6efbe1830bea232c3a081
voitel2109@gmail.com <voitel2109@gmail.com>
2021-05-24 17:32:38 +0300

    1

--- BrowserTesting/Pages/ComputerPage.cs
+++ BrowserTesting/Pages/ComputerPage.cs
@@ -15,6 +15,13 @@ namespace BrowserTesting
 {
     class ComputerPage : OrderPage
     {
+        readonly string defaultProcessor = "2.5 GHz Intel Pentium Dual-Core E2200 [+15.00]";
+        readonly string defaultRAM = "2 GB";
+        readonly string defaultHDD = "";
+        readonly string defaultOS = "Ubuntu";
+        readonly string[] defaultSoftware = new string[] {"Microsoft Office [+50.00]" };
+        readonly string defaultCount = "1";
+        readonly string defaultPriceColor = Colors.Red;
         public ComputerPage(IWebDriver Driver) : base(Driver)
         {
 
@@ -31,17 +38,17 @@ namespace BrowserTesting
             IWebElement list = Driver.FindElement(By.XPath(path));
             return list;
         }
-        public string CheckDefaultProcessor()
+        public void CheckDefaultProcessor()
         {
             SelectElement list = new SelectElement(ProcessorList());
             string nameProcessor = list.SelectedOption.Text;
-            return nameProcessor;
+            Assert.AreEqual(defaultProcessor, nameProcessor, "Фактический процессор не соответствует требуемому по умолчанию");
         }
-        public string CheckDefault_RAM()
+        public void CheckDefault_RAM()
         {
             SelectElement list = new SelectElement(RAM_List());
             string nameRAM = list.SelectedOption.Text;
-            return nameRAM;
+            Assert.AreEqual(defaultRAM, nameRAM, "Фактический RAM не соответствует требуемому по умолчанию");
         }
         public ReadOnlyCollection<IWebElement> HDD_Buttons()
         {
@@ -49,11 +56,25 @@ namespace BrowserTesting
             ReadOnlyCollection<IWebElement> list = Driver.FindElements(By.XPath(path));
             return list;
         }
-        public string CheckDefault_HDD()
+        public void CheckDefault_HDD()
         {
-            string nameHDD = "Значение по умолчанию для HDD не установлено";
-            int count = 1;
-            foreach (IWebElement parameter in HDD_Buttons())
+            string nameHDD = "";
+            ReadOnlyCollection<IWebElement> listHDD = HDD_Buttons();
+            int i = 0;
+            bool check = true;
+            while (i < listHDD.Count && check)
+            {
+                if (listHDD[i].Selected)
+                {
+                    nameHDD = listHDD[i].FindElement(
+                        By.XPath($"(//dl//dt//label[normalize-space(text())='HDD']//../following::dd[1]//li/label)[{i+1}]"))
+                        .Text;
+                    Assert.AreEqual(defaultHDD, nameHDD, "Фактический HDD не соответствует требуемому по умолчанию");
+                    check = false;
+                }
+                i++;
+            }
+            /*foreach (IWebElement parameter in HDD_Buttons())
             {
                 if (parameter.Selected)
                 {
@@ -62,8 +83,7 @@ namespace BrowserTesting
                         .Text;
                 }
                 count++;
-            }
-            return nameHDD;
+            }*/
         }
         public ReadOnlyCollection<IWebElement> OS_Buttons()
         {
@@ -71,11 +91,29 @@ namespace BrowserTesting
             ReadOnlyCollection<IWebElement> list = Driver.FindElements(By.XPath(path));
             return list;
         }
-        public string CheckDefault_OS()
+        /// <summary>
+        /// Проверка выбранной RadioButton для списка OS,
+        /// возврат названия параметра соответсвтующей RadioButton
+        /// </summary>
+        public void CheckDefault_OS()
         {
-            string nameOS = "Значение по умолчанию для OS не установлено";
-            int count = 1;
-            foreach (IWebElement parameter in OS_Buttons())
+            string nameOS = "";
+            ReadOnlyCollection<IWebElement> listOS = OS_Buttons();
+            int i = 0;
+            bool check = true;
+            while (i < listOS.Count && check)
+            {
+                if (listOS[i].Selected)
+                {
+                    nameOS = listOS[i].FindElement(
+                        By.XPath($"(//dl//dt//label[normalize-space(text())='OS']//../following::dd[1]//li/label)[{i + 1}]"))
+                        .Text;
+                    Assert.AreEqual(defaultOS, nameOS, "Фактическая OS не соответствует требуемой по умолчанию");
+                    check = false;
+                }
+                i++;
+            }
+            /*foreach (IWebElement parameter in OS_Buttons())
             {
                 if (parameter.Selected)
                 {
@@ -85,45 +123,72 @@ namespace BrowserTesting
                 }
                 count++;
             }
-            return nameOS;
+            return nameOS;*/
         }
+        /// <summary>
+        /// Поиск и формирование списка доступных параметров для выбора у Software
+        /// </summary>
         public ReadOnlyCollection<IWebElement> SoftwareList()
         {
             string path = "//dl//dt//label[normalize-space(text())='Software']//../following::dd[1]//li/input";
             ReadOnlyCollection<IWebElement> list = Driver.FindElements(By.XPath(path));
             return list;
         }
-        public List<string> CheckDefaultSoftware()
+        /// <summary>
+        /// Проверка выбранных элементов в Checkbox при списке Software,
+        /// возврат списка выбранных элементов
+        /// </summary>
+        public void CheckDefaultSoftware()
         {
-            List<string> defaultSoftware = new List<string>();
+            List<string> listSoftware = new List<string>();
             int count = 1;
             foreach(IWebElement parameter in SoftwareList())
             {
                 if (parameter.Selected)
                 {
-                    defaultSoftware.Add(Driver.FindElement
+                    listSoftware.Add(Driver.FindElement
                         (By.XPath($"(//dl//dt//label[normalize-space(text())='Software']//../following::dd[1]//li/label)[{count}]"))
                         .Text);
                 }
             }
-            return defaultSoftware;
+            for (int i = 0; i < listSoftware.Count; i++)
+            {
+                for (int j = 0; j < defaultSoftware.Length;j++)
+                {
+                    Assert.AreEqual(defaultSoftware[j], listSoftware[i], 
+                        "Фактический параметр Software не соответствует требуемому по умолчанию");
+                }
+            }
         }
-        public string CheckDefaultCount()
+        /// <summary>
+        /// Проверка значения количества по умолчанию в соответствующем поле,
+        /// возврат этого значения ввиде строки
+        /// </summary>
+        public void CheckDefaultCount()
         {
             SetItemCountField();
             string itemCount = Driver.FindElement(itemCountField).GetAttribute("value");
-            return itemCount;
+            Assert.AreEqual(defaultCount, itemCount,
+                "Фактическое значение параметра Qty не соответствует требуемому по умолчанию");
         }
-        public bool CheckPriceColor_RED()
+        /// <summary>
+        /// Проверка соответствия фактического цвета цены товара красному цвету, 
+        /// предусмотрен Assert при несоответствии
+        /// </summary>
+        public void CheckPriceColor_RED()
         {
             String buttonTextColor = Driver.FindElement(By.XPath("//span[@itemprop='price']")).GetCssValue("color");
-            Assert.AreEqual(Colors.Red, buttonTextColor, "Цвет цены предмета не красный");
-            return buttonTextColor == Colors.Red;
+            Assert.AreEqual(defaultPriceColor, buttonTextColor, "Цвет цены предмета не красный");
+            //return buttonTextColor == Colors.Red;
         }
+        ///<summary>
+        ///Главная функция проверки парамаетров по умолчанию 
+        ///для страницы "Build your own computer"
+        ///</summary>
         public void StartCheckDefaultParameters()
         {
             List<string> defaultParameters = new List<string>();
-            defaultParameters.Add(CheckDefaultProcessor());
+            /*defaultParameters.Add(CheckDefaultProcessor());
             defaultParameters.Add(CheckDefault_RAM());
             defaultParameters.Add(CheckDefault_HDD());
             defaultParameters.Add(CheckDefault_OS());
@@ -131,8 +196,14 @@ namespace BrowserTesting
             foreach(string n in CheckDefaultSoftware())
             {
                 defaultParameters.Add(n);
-            }
-            CheckPriceColor_RED();
+            }*/
+            CheckDefaultProcessor();
+            CheckDefault_RAM();
+            CheckDefault_HDD();
+            CheckDefault_OS();
+            CheckDefaultCount();
+            CheckDefaultSoftware();
+            //CheckPriceColor_RED();
         }
     }
 }
